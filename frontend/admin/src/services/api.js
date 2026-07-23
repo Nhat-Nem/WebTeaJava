5184a0842a9bdd11fbb2901421099bd8362ec73b
Nhat-Nem <bnam64305@gmail.com>
2026-07-23 09:38:29 +0700

    fix api.js

--- frontend/admin/src/services/api.js
+++ frontend/admin/src/services/api.js
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
     baseURL: import.meta.env.VITE_SERVER_API,
     withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
 
