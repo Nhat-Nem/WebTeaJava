8f4277a68b51cb0669ddd34b914919cf6b6c4562
Nhat-Nem <bnam64305@gmail.com>
2026-07-24 18:56:00 +0700

    add synatx

--- backend/projectweb/src/main/java/com/java/projectweb/Config/WebConfig.java
+++ backend/projectweb/src/main/java/com/java/projectweb/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String uploadDir = System.getProperty("user.dir") + "/uploads/";
 
-        System.out.println("RESOURCE = file:" + uploadDir)
+        System.out.println("RESOURCE = file:" + uploadDir);
 
         registry.addResourceHandler("/products/**")
                 .addResourceLocations("file:uploads/products/");
